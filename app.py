6cce89e0a38ca3c9668188507f5493f3662c8fd0
Kulolamathias <kalolamathias@gmail.com>
2023-09-29 09:50:01 +0300

    first changes

---
+++ app.py
@@ -0,0 +1,17 @@
+x=4
+y=8
+
+def add():
+ 
+ z = x+y
+ print("the diffference is: ", z)
+
+def diff():
+ if x<y:
+  z=y-x
+  print("the diffference is: ", z)
+ elif y< x:
+  z=x-y
+  print("the diffference is: ", z)
+ else:
+  print("the numbers are equal hence zero difference") 
